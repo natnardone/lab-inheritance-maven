298ac62b41220f8c5b5ef97a8b41287c14798fb8
Natalie Nardone <nardonen@grinnell.edu>
2024-10-01 09:08:19 -0500

    Add Tally class and corresponding experiments

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -2,6 +2,8 @@ package edu.grinnell.csc207.experiments;
 
 import edu.grinnell.csc207.util.Counter;
 
+import edu.grinnell.csc207.util.Tally;
+
 import java.io.PrintWriter;
 
 import edu.grinnell.csc207.util.BasicCounter;
@@ -21,7 +23,7 @@ public class CounterExperiments {
 
 
     // Set up some counters
-    Counter alpha = new BasicCounter(0);
+    Counter alpha = new Tally();
     Counter beta = new BasicCounter(123);
     Counter gamma = new BasicCounter(-5);
 
---
+++ src/main/java/edu/grinnell/csc207/util/Tally.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207.util;
+
+public class Tally extends BasicCounter {
+  /**
+   * Create a new Tally.
+   *
+   * @param start
+   *   The initial value of the tally.
+   */
+  public Tally() {
+    super(0);
+  } // Tally(int)
+} // class Tally
